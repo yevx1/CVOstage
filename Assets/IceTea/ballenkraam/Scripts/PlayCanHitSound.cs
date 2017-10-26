24a00686003f45c5be65e02e2f6ff940ac566ef6
yves <yvesvangestelen@gmail.com>
2017-10-26 18:06:19 +0200

    commit door gerald

---
+++ Assets/IceTea/ballenkraam/Scripts/PlayCanHitSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Valve.VR.InteractionSystem
+{
+    public class PlayCanHitSound : MonoBehaviour
+    {
+        public SoundPlayOneshot collisionSound;
+        
+        private void OnCollisionEnter(Collision collision)
+        {
+            
+                    collisionSound.Play();
+                    
+
+                
+        }
+    }
+}
